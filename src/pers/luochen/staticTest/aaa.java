0a325b1632d16850440fd2214ecc71fa454b1e7b
Jack <2392305063@qq.com>
2022-04-06 12:58:26 +0800

    Initial Commit. Work hard, it's not too late.

---
+++ src/pers/luochen/staticTest/aaa.java
@@ -0,0 +1,11 @@
+package pers.luochen.staticTest;
+
+/**
+ * @author LiZhiHao
+ * @date 2022/4/6 12:57
+ */
+public class aaa {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
