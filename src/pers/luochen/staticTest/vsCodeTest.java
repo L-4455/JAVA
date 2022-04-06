25498cd38a30be6664f39a9d9607e83e5577d70f
落尘 <2382395063@qq.com>
2022-04-06 17:32:00 +0800

    Initial commit

---
+++ src/pers/luochen/staticTest/vsCodeTest.java
@@ -0,0 +1,13 @@
+/**
+ * @author: LiZhiHao
+ * @Date: 2022-04-06 17:30:50
+ * @LastEditTime: 2022-04-06 17:31:21
+ */
+package pers.luochen.staticTest;
+
+public class vsCodeTest {
+public static void main(String[] args) {
+    System.out.println("HelloWorld");
+}
+    
+}
