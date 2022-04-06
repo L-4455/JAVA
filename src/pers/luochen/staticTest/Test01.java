412c940da7a0a1c5e77a055be1132025ac044e7a
落尘 <2382395063@qq.com>
2022-04-06 17:37:16 +0800

    Initial commit

---
+++ src/pers/luochen/staticTest/Test01.java
@@ -0,0 +1,13 @@
+/**
+ * @author: LiZhiHao
+ * @Date: 2022-04-06 17:36:59
+ * @LastEditTime: 2022-04-06 17:37:01
+ */
+package pers.luochen.statictest;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("AA");
+    }
+    
+}
