775f30791f020fba71085c247ba9c6ea213d5b7d
Jack <2392305063@qq.com>
2022-04-06 14:35:35 +0800

    Vscode提交

---
+++ src/pers/luochen/staticTest/www.java
@@ -0,0 +1,13 @@
+/**
+ * @author: LiZhiHao
+ * @Date: 2022-04-06 14:34:11
+ * @LastEditTime: 2022-04-06 14:34:12
+ */
+package pers.luochen.staticTest;
+
+
+public class www {
+    public static void main(String[] args) {
+        System.out.println("wo");
+    }
+}
