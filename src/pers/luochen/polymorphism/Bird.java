c96fc2377698ab279c0b9a5b1b68f03a958ebeab
落尘 <2382395063@qq.com>
2022-04-07 22:53:44 +0800

    Initial Commit.

--- src/pers/luochen/polymorphism/Bird.java
+++ src/pers/luochen/polymorphism/Bird.java
@@ -1,15 +1,15 @@
 /**
  * @author: LiZhiHao
  * @Date: 2022-04-07 22:20:21
- * @LastEditTime: 2022-04-07 22:48:36
+ * @LastEditTime: 2022-04-07 22:53:36
  */
 package pers.luochen.polymorphism;
 
 
 public class Bird extends Animal {
 
-    // @Override
-    // public void move() {
-    //     System.out.println("鸟儿在飞翔");
-    // }
+    @Override
+    public void move() {
+        System.out.println("鸟儿在飞翔");
+    }
 }
