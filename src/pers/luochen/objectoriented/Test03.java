94dc3148e931088d6be40c7090dae7ebb63e5a16
落尘 <2382395063@qq.com>
2022-04-09 23:54:24 +0800

    Initial Commit. Work hard, it's not too late.

--- src/pers/luochen/objectoriented/Test03.java
+++ src/pers/luochen/objectoriented/Test03.java
@@ -1,13 +1,12 @@
+package pers.luochen.objectoriented;
+
 /**
  * @author: LiZhiHao
  * @Date: 2022-03-26 19:04:00
  * @LastEditTime: 2022-03-27 22:28:29
  */
-
-package pers.luochen.objectoriented;
-
-public class Ootese03 {
-    public static void main (String [] args){
+public class Test03 {
+    public static void main(String[] args) {
         //实例化User对象
         User u = new User();
         //实例化Adders对象
@@ -19,7 +18,5 @@ public class Ootese03 {
         a.city = "河南";
         //河南
         System.out.println(u.addr.city);
-
-
     }
 }
