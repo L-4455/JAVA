7507b6022e0da5adf409834822cf0166d6e973c3
Jack <l2382395063@gmail.com>
2022-05-11 20:11:00 +0800

    Initial Commit.

---
+++ src/pers/object/tostring/Test01.java
@@ -0,0 +1,9 @@
+package pers.object.tostring;
+
+/**
+ * @Author LiZiHao
+ * @Date 2022/5/9/23:22
+ */
+public class Test01 {
+    private int age;
+}
---
+++ src/pers/object/tostring/ToString.java
@@ -0,0 +1,16 @@
+package pers.object.tostring;
+
+/**
+ * @Author LiZiHao
+ * @Date 2022/5/9/23:19
+ */
+public class ToString {
+    public static void main(String[] args) {
+        /*
+         * toString的使用
+         *
+         * */
+        Test01 test01 = new Test01();
+        System.out.println(test01.toString());
+    }
+}
