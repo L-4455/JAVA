bb6ff8cf911b4581c97c12ba0f63b0ebc537f325
Jack <l2382395063@gmail.com>
2022-05-23 00:11:01 +0800

    Initial Commit.

---
+++ src/pers/array/ArrayTest07.java
@@ -0,0 +1,89 @@
+package pers.array;
+
+/**
+ * @Author LiZiHao
+ * @Date 2022/5/22/21:52
+ */
+public class ArrayTest07 {
+    /*
+        数组中的元素保存的是引用时该怎么办?
+        数组中保存的元素不会是java对象而是java对象的内存地址.
+     */
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Animal animal1 = new Animal();
+        Animal[] animals = {animal, animal1};
+        for (int i = 0; i < animals.length; i++) {
+            /*Animal a = animals[i];
+            a.move();*/
+//            合并,animals[i]中保存的是数组中的java对象的内存地址.animals[i]不是数组,而是数组中保存的元素
+            animals[i].move();
+        }
+        System.out.println("==================");
+        Animal[] animals1 = new Animal[2];
+        for (int i = 0; i < animals1.length; i++) {
+            animals1[0] = new Bird();
+//            不兼容的类型: pers.array.person无法转换为pers.array.Animal
+//            Animal类型的数组只能保存Animal类型的.
+//            animals1[1] = new person();
+
+//            子类可以保存.一个类的子类可以保存在夫类的数组中
+            animals1[1] = new Cat();
+            animals1[i].move();
+        }
+        System.out.println("+++++++++++++++++++++++++++++");
+
+//        创建一维数组存放Cat和Bird.
+        Animal[] animal2 = {new Cat(), new Bird()};
+        for (int i = 0; i < animal2.length; i++) {
+//            不兼容的类型: pers.array.Animal无法转换为pers.array.Cat
+//            animal2[0]是Animal类型的数组元素,无法转换为Cat类型的
+//            Cat cat = animal2[0];
+            Animal animal3 = animal2[i];
+//                调用move方法,因为Animal中有move,子类重写了move方法,所以不需要向下转型.
+//                animal3.move();
+
+            if (animal2[i] instanceof Cat) {
+                Cat cat = (Cat) animal2[i];
+                cat.catMouse();
+            } else if (animal2[i] instanceof Bird) {
+                Bird bird = (Bird) animal2[i];
+                bird.birdFlay();
+            }
+        }
+    }
+}
+
+class Animal {
+    public void move() {
+        System.out.println("Animal move");
+    }
+}
+
+class person {
+
+}
+
+class Cat extends Animal {
+    public void move() {
+        System.out.println("Cat move");
+    }
+
+    //    Cat特有的方法
+    public void catMouse() {
+        System.out.println("猫抓老鼠");
+    }
+}
+
+class Bird extends Animal {
+    public void move() {
+        System.out.println("Bird move");
+    }
+
+    //    Bird特有的方法
+    public void birdFlay() {
+        System.out.println("Bird flay");
+    }
+}
+
+
---
+++ src/pers/array/ArrayTest08.java
@@ -0,0 +1,57 @@
+package pers.array;
+
+/**
+ * @Author LiZiHao
+ * @Date 2022/5/22/22:44
+ */
+public class ArrayTest08 {
+    /*
+        数组怎么扩容?
+        在java中数组长度一旦确定就不可改变,那么当数组中的保存元素的长度大于数组所定的长度,怎么办?怎么扩容?
+        在java中数组扩容:
+            新建一个数组,确定新数组的长度合适.把要扩容的数组拷贝到新数组中.
+        结论: 数组拷贝效率低,因为需要一个一个的元素去拷贝进去,当源元素多的话,拷贝的时间就长.所以在开发中,尽可能的预计数组的长度.
+              预计要尽可能的准确,即使拷贝了,也要减少拷贝的次数.
+        JAVA中system类中提供了arraycopy方法,用来拷贝数组.
+        public static native void arraycopy(Object src,  int  srcPos,
+                                        Object dest, int destPos,
+                                        int length);
+        在这个方法中需要传递5个参数,分别是:
+                                    拷贝源,拷贝起始位置,拷贝目标,拷贝目标位置,拷贝长度.
+
+        拷贝的元素可以是引用,但拷贝进去的是对象的内存地址.不会是java对象.拷贝不会再次创建对象.
+     */
+    public static void main(String[] args) {
+        int[] number = {123, 34, 512, 456, 112};
+        int[] num = new int[10];
+//        拷贝源number,从第0个下标开始拷贝,拷贝位置num,从第0个下标开始拷贝,拷贝长度是number的长度
+        System.arraycopy(number, 0, num, 0, number.length);
+//        遍历num中的元素,前几个个元素都是从其他地方拷贝进来的.具体是几个元素要看源目标的长度.
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+        System.out.println("======================");
+//        拷贝引用数据类型
+        String[] name = {"lisi", "hangman", "LiZiHao", "hh", "qaq", "query", "beware", "array"};
+        String[] username = new String[20];
+        System.out.println("name数组的长度:" + name.length + " \n元素有:");
+        System.arraycopy(name, 0, username, 0, name.length);
+        for (int i = 0; i < name.length; i++) {
+            System.out.print(name[i] + " ");
+        }
+        System.out.println();
+        System.out.println("拷贝到num中,拷贝长度为:" + name.length);
+        for (int i = 0; i < username.length; i++) {
+            System.out.println("拷贝第" + i + "个" + username[i]);
+        }
+        System.out.println("====================");
+        Object[] object = {"13", "234", "2334", "324"};
+        Object[] objects = new Object[10];
+        System.arraycopy(object, 0, objects, 0, object.length);
+        for (int i = 0; i < objects.length; i++) {
+            System.out.println(objects[i]);
+        }
+
+
+    }
+}
