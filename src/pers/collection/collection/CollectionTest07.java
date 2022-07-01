ddd09e85713231a36d046f65f787a0425091af42
落尘 <l2382395063@gmail.com>
2022-07-01 11:19:43 +0800

    initial commit.

--- src/pers/collection/collection/CollectionTest07.java
+++ src/pers/collection/collection/CollectionTest07.java
@@ -1,4 +1,4 @@
-package pers.collection;
+package pers.collection.collection;
 
 import java.util.ArrayList;
 import java.util.Collection;
---
+++ src/pers/collection/danlink/Link.java
@@ -0,0 +1,8 @@
+package pers.collection.danlink;
+
+/**
+ * @Author LiZiHao
+ * @Date 2022/7/1/11:18
+ */
+public class Link {
+}
