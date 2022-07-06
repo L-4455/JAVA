79b6ccd283fb5672f10e8db21cb1dbc9215de1cd
落尘 <l2382395063@gmail.com>
2022-07-06 16:17:11 +0800

    initial commit.

---
+++ src/pers/collections/CollectionsTest.java
@@ -0,0 +1,78 @@
+package pers.collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @Author LiZiHao
+ * @Date 2022/07/06/16:00
+ */
+public class CollectionsTest {
+    /*
+        集合工具类
+     */
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+//        把list集合转为线程安全的
+        Collections.synchronizedList(list);
+
+        list.add("123");
+        list.add("200");
+        list.add("2000");
+        list.add("21312");
+//        对list集合进行排序,升序.
+        Collections.sort(list);
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+//        对自定义类进行使用short排序
+        List<Monkey> list1 = new ArrayList<>();
+        list1.add(new Monkey(10));
+        list1.add(new Monkey(30));
+        list1.add(new Monkey(5));
+
+//        编译报错,不符合类型.Monkey没有实现Comparable接口
+//        Collections.sort(list1);
+//        凡是自定义类型要进行排序,都要实现Comparable接口.
+        Collections.sort(list1);
+        for (Monkey monkey : list1) {
+            System.out.println(monkey);
+        }
+//      sort也可以使用比较器。
+        Collections.sort(list1, new MonkeyComparator());
+        for (Monkey monkey : list1) {
+            System.out.println(monkey);
+        }
+    }
+}
+
+class Monkey implements Comparable<Monkey> {
+    int age;
+
+    public Monkey(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Monkey{" +
+                "age=" + age +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Monkey o) {
+        return this.age - o.age;
+    }
+}
+
+class MonkeyComparator implements Comparator<Monkey> {
+    @Override
+    public int compare(Monkey o1, Monkey o2) {
+        return o2.age - o1.age;
+    }
+}
